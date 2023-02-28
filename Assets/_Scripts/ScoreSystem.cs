43ff2d63546782a2bb3c8bb5efa52ea4e99f33e7
aeroxman <brasil_denis@hotmail.com>
2023-02-28 17:26:57 -0500

    Collectable Prefab
    collectables, score and particle for collectable

--- Assets/_Scripts/ScoreSystem.cs
+++ Assets/_Scripts/ScoreSystem.cs
@@ -7,15 +7,33 @@ public class ScoreSystem : MonoBehaviour
 {
     public GameObject scoreText;
     public static int theScore;
-    [SerializeField] public int completeLevel;
 
-    public GameObject endLevel;
+    public ParticleSystem collisionParticleSystem;
+    public MeshRenderer mr;
+    public bool once = true;
 
     void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Player") && once)
+        {
+            var em = collisionParticleSystem.emission;
+            var dur = collisionParticleSystem.duration;
+
+            em.enabled = true;
+            collisionParticleSystem.Play();
+
+            once = false;
+            Destroy(mr);
+            Invoke(nameof(DestroyObj), dur);
+        }
+
         theScore++;
         scoreText.GetComponent<Text>().text = "" + theScore;
-        Destroy(gameObject);
         //transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    void DestroyObj()
+    {
+        Destroy(gameObject);
     }
 }
